0d910748cd2c96828447b5408b25ea8e33138d2c
192110684 <113339282+192110684@users.noreply.github.com>
2023-09-11 13:29:42 +0530

    Add files via upload

---
+++ s4.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int i;
+    printf("the numbers:");
+					for(i=10;i>=1;i--)
+					{
+ 							printf("%d \n",i);
+ 						
+					}
+	 printf("\n");
+ return 0;
+}
