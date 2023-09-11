6b5533a20e6388286c88fed2f7ed3f4b259b03b2
192110684 <113339282+192110684@users.noreply.github.com>
2023-09-11 13:25:51 +0530

    Add files via upload

---
+++ c-1.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	int num;
+	printf("enter the value");
+	scanf("%d",&num);
+	if(num%2==0)
+	printf("%d number is even",num);
+	else
+	printf("%d not a even number",num);
+	return 0;
+}
